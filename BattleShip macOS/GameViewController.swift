4ee9abd7aa71c3316a018c0c7e37756587159e34
Christian Muth <christian-muth@t-online.de>
2018-12-04 13:14:13 +0100

    Initial Commit

---
+++ BattleShip macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  BattleShip macOS
+//
+//  Created by Christian Muth on 04.12.18.
+//  Copyright © 2018 Christian Muth. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
