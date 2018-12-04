15710d3a9edc77821e1b105fad50b584b6da3f4e
Christian Muth <christian-muth@t-online.de>
2018-12-04 13:14:13 +0100

    Initial Commit

---
+++ BattleShip macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BattleShip macOS
+//
+//  Created by Christian Muth on 04.12.18.
+//  Copyright © 2018 Christian Muth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
