f2b31469302334959ef33859ef1d60671e860d8c
Christian Muth <christian-muth@t-online.de>
2019-10-13 13:18:58 +0200

    die MacOS Version war noch nicht auf dem Stand wie die iOS Version. user Aktionen ausserhalb des Spielfeldes wurden nicht immer korrekt verarbeitet. die Warnungen über unbenutzte lokale Variablen wurden entfernt (auskommentiert). Schlüsselwort public entfernt, wo es nicht mehr nötig ist bei Swift 5. .sks (scenes) in  eine eigene Gruppe gepackt.

--- BattleShip Shared/GameScene.swift
+++ BattleShip Shared/GameScene.swift
@@ -21,6 +21,11 @@ class GameScene : SKScene {
 
     var gamePlayFieldPosition = CGPoint.zero
     
+    // die Angaben in gamePlayFieldKoordinaten (nicht in Scene Koordinaten)
+    var playGameFieldPositionLocal = CGPoint(x: 0, y: 0)
+    var playGameFieldRectLocal = CGRect(x: 0, y: 0, width: 0, height: 0)
+    var playGameFieldSizeLocal = CGSize(width: 0, height: 0)
+    
     var gridSize = 0
 
     var securedFields:[FieldIndex] = []
@@ -72,6 +77,45 @@ class GameScene : SKScene {
         addNumberLabels()
         calculateGamePlayFieldPosition()
         gamePlayField.position = gamePlayFieldPosition
+        
+        // berechne hier die erforderlichen geometrischen Größen in playGameField - Koordinaten
+        playGameFieldPositionLocal = self.convert(gamePlayField.position, to: gamePlayField)
+
+        let lowerLeftGamePlayFieldX = gamePlayField.position.x - gamePlayField.mapSize.width/2
+        let lowerLeftGamePlayFieldY = gamePlayField.position.y - gamePlayField.mapSize.height/2
+        let lowerLeftGamePlayFielPosition = CGPoint(x: lowerLeftGamePlayFieldX, y: lowerLeftGamePlayFieldY)
+        let lowerLeftGamePlayFieldPositionLocal = self.convert(lowerLeftGamePlayFielPosition, to: gamePlayField)
+        
+
+        let upperLeftGamePlayFieldX = gamePlayField.position.x - gamePlayField.mapSize.width/2
+        let upperLeftGamePlayFieldY = gamePlayField.position.y + gamePlayField.mapSize.height/2
+        let upperLeftGamePlayFielPosition = CGPoint(x: upperLeftGamePlayFieldX, y: upperLeftGamePlayFieldY)
+        let upperLeftGamePlayFieldPositionLocal = self.convert(upperLeftGamePlayFielPosition, to: gamePlayField)
+
+        let lowerRightGamePlayFieldX = gamePlayField.position.x + gamePlayField.mapSize.width/2
+        let lowerRightGamePlayFieldY = gamePlayField.position.y - gamePlayField.mapSize.height/2
+        let lowerRightGamePlayFielPosition = CGPoint(x: lowerRightGamePlayFieldX, y: lowerRightGamePlayFieldY)
+        let lowerRightGamePlayFieldPositionLocal = self.convert(lowerRightGamePlayFielPosition, to: gamePlayField)
+
+        let upperRightGamePlayFieldX = gamePlayField.position.x + gamePlayField.mapSize.width/2
+        let upperRightGamePlayFieldY = gamePlayField.position.y + gamePlayField.mapSize.height/2
+        let upperRightGamePlayFieldPosition = CGPoint(x: upperRightGamePlayFieldX, y: upperRightGamePlayFieldY)
+        let upperRightGamePlayFieldPositionLocal = self.convert(upperRightGamePlayFieldPosition, to: gamePlayField)
+        
+        let localWidth = upperRightGamePlayFieldPositionLocal.x - lowerLeftGamePlayFieldPositionLocal.x < 0 ?
+            -(upperRightGamePlayFieldPositionLocal.x - lowerLeftGamePlayFieldPositionLocal.x) :
+            upperRightGamePlayFieldPositionLocal.x - lowerLeftGamePlayFieldPositionLocal.x
+
+        let localHeight = upperRightGamePlayFieldPositionLocal.y - lowerLeftGamePlayFieldPositionLocal.y < 0 ?
+            -(upperRightGamePlayFieldPositionLocal.y - lowerLeftGamePlayFieldPositionLocal.y) :
+            upperRightGamePlayFieldPositionLocal.y - lowerLeftGamePlayFieldPositionLocal.y
+        
+        playGameFieldRectLocal = CGRect(x: lowerLeftGamePlayFieldPositionLocal.x, y: lowerLeftGamePlayFieldPositionLocal.y, width: localWidth, height: localHeight)
+        playGameFieldSizeLocal = playGameFieldRectLocal.size
+
+        print("lowerLeft = ",lowerLeftGamePlayFieldPositionLocal,"  uperLeft = ",upperLeftGamePlayFieldPositionLocal,
+              "lowerRight = ",lowerRightGamePlayFieldPositionLocal,"  uperRight = ",upperRightGamePlayFieldPositionLocal)
+        
         
         addChild(gamePlayField)
     }
